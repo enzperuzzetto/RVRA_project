af7a12e1b899568029d97eddc5ee86b29ac43af5
Teiki Pepin <teiki.pepin@etu.u-bordeaux.fr>
2018-12-05 17:55:48 +0100

    Assets and stuff

---
+++ Assets/Scripts/Geocentric_Rotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Geocentric_Rotation : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime * speed);
+    }
+}
