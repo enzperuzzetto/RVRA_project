149bd73e8d9ba63da3ecc2f8fb5edad3800d60ad
rembrisset <remi.brisset@etu.u-bordeaux.fr>
2018-12-11 16:30:21 +0100

    maj displayScene

--- Assets/Scripts/displayScene.cs
+++ Assets/Scripts/displayScene.cs
@@ -6,42 +6,36 @@ public class displayScene : MonoBehaviour {
 	public GameObject marker1;
 	private GameObject background;
 
-    private Vector3 startingScale;
-
-    private Vector3 prevPos0;
-    private Vector3 currPos0;
-    private Vector3 prevPos1;
-    private Vector3 currPos1;
     private float passedTime;
+
+    private const float UPDATE_TIME = 0.5f;
 
     // Use this for initialization
     void Start ()
     {
         background = GameObject.Find("background");
-        startingScale = background.transform.localScale;
-
-        prevPos0 = Vector3.one;
-        currPos0 = Vector3.one;
-        prevPos1 = Vector3.one;
-        currPos1 = Vector3.one;
-        passedTime = Time.deltaTime; 
+        float passedTime = 0;
+         
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-        currPos0 = marker0.transform.position;
-        currPos1 = marker1.transform.position;
 
-        passedTime = isVisible(passedTime, background, currPos0, prevPos0, currPos1, prevPos1);
-
-        prevPos0 = currPos0;
-        prevPos1 = currPos1;
+        if (marker0.activeSelf && marker1.activeSelf){
+            background.SetActive(true);
+        }
+        else{
+            background.SetActive(false);
+        }
 
 		float dist = distance(marker0, marker1);
-		setPosition(background, marker0, marker1);
-        background.transform.localScale = new Vector3(dist * 0.1f, dist * 0.1f, dist * 0.1f);
-		
+        //Vector3 rotation = mean_rotation(marker0, marker1);
+        passedTime += Time.deltaTime;
+        if (passedTime > UPDATE_TIME){
+		    setPosition(background, marker0, marker1);
+            background.transform.localScale = new Vector3(dist * 0.1f, dist * 0.1f, dist * 0.1f);
+        }
 	}
 
 	void setPosition( GameObject toset, GameObject obj1, GameObject obj2)
@@ -63,31 +57,10 @@ public class displayScene : MonoBehaviour {
 		return Mathf.Sqrt ((x * x) + (y * y) + (z * z));
 	}
 
-    /*
-     * Check if gameObject is changing position
-     */
-    bool isMoving(Vector3 pos0, Vector3 pos1){
-        return (pos0!=pos1);
-
-    }
-
-    /*
-     * Display the GameObject if his position has changed in the last half second
-     */
-    float isVisible(float passedTime, GameObject obj, Vector3 pos0, Vector3 prev0, Vector3 pos1, Vector3 prev1)
-    {
-        if (!isMoving(prev0, pos0) || !isMoving(prev1, pos1)){
-            passedTime += Time.deltaTime;
-            if (passedTime > 0.5){
-                obj.SetActive(false);    
-            }
-        }
-        else{
-            obj.SetActive(true);
-            passedTime = 0;
-        }
-        return passedTime;
-    }
-
+    // Vector3 mean_rotation(GameObject obj1, GameObject obj2){
+    //     Vector3 mean = new Vector3(obj1.transform.rotation)
+    //     return 
+    // }
+  
 
 }
