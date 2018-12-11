df90ba6f410f28fe9365e9d38595bb5561d101ef
Teiki Pepin <teiki.pepin@etu.u-bordeaux.fr>
2018-12-11 18:42:01 +0100

    Rotation

--- Assets/Scripts/displayPlanet.cs
+++ Assets/Scripts/displayPlanet.cs
@@ -6,6 +6,7 @@ public class displayPlanet : MonoBehaviour
 {
 	public List<GameObject> markers;
 	public List<GameObject> planets;
+	public List<float> rot = new List<float>();
 
     private float transferCooldown;
 	private const float COOLDOWN_VALUE = 2.0f;
@@ -18,9 +19,11 @@ public class displayPlanet : MonoBehaviour
 	{
 		for (int i = 0; i < markers.Count; i++) {
 			passedTime.Add(.0f);
+			rot.Add(.0f);
 			if (planets[i] != null) {
 				markers[i].transform.position = new Vector3(3.0f * i, 3.0f * i, 3.0f * i);
 				planets[i].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+				setParent(planets[i], markers[i]);
 			}
 		}
 		transferCooldown = COOLDOWN_VALUE;
@@ -33,15 +36,16 @@ public class displayPlanet : MonoBehaviour
 
 		for (int i = 0; i < markers.Count; i++) {
 			if (planets[i] != null) {
-				planets [i].SetActive (true);
-				passedTime[i] = updatePosition(passedTime[i], planets[i], markers[i]);			
+				planets[i].SetActive (true);
+				passedTime[i] = updatePosition(passedTime[i], planets[i], markers[i], rot[i]);
+				rot[i] = (rot[i] + 1.0f) % 360.0f;
 			}
 			else {
 				for (int j = 0; j < markers.Count; j++) {
 					if (planets[j] != null && transferCooldown == .0f && distance(markers[i], markers[j]) < TRANSFER_DIST_MIN) {
 						planets[i] = planets[j];
 						planets[j] = null;
-						setPosition(planets[i], markers[i]);
+						setParent(planets[i], markers[i]);
 						transferCooldown = COOLDOWN_VALUE;
 						break;
 					}
@@ -51,10 +55,16 @@ public class displayPlanet : MonoBehaviour
 	}
 
 
-	void setPosition(GameObject planet, GameObject marker)
+	void setParent(GameObject planet, GameObject marker)
 	{
 		planet.transform.parent = marker.transform;
 		planet.transform.localPosition = new Vector3 (.0f, 0.05f, .0f);
+	}
+	
+	void setPosition(GameObject planet, GameObject marker, float rot)
+	{
+		planet.transform.position = marker.transform.position;
+		planet.transform.rotation = Quaternion.Euler(0, rot, 0);
 	}
 
     /*
@@ -72,12 +82,13 @@ public class displayPlanet : MonoBehaviour
 	 *	Update Position of obj according to obj2 if it has not been update 
 	 *  in the last half second 
 	 */
-    float updatePosition(float passedTime, GameObject obj, GameObject obj2)
+    float updatePosition(float passedTime, GameObject planet, GameObject marker, float rot)
     {
         passedTime += Time.deltaTime;
-        if (passedTime > 0.5){
-        	setPosition(obj, obj2);   
-		}
+
+        if (passedTime > 0.5)
+        	setPosition(planet, marker, rot);
+
         return passedTime;
     }
 
